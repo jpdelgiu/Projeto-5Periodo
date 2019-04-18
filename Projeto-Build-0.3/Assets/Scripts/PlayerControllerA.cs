2d6073bdb915446c2eb32cb45486455e4ba9adb6
jpdelgiu <49752852+jpdelgiu@users.noreply.github.com>
2019-04-18 17:04:13 -0300

    Build 0.3
    18/04

---
+++ Projeto-Build-0.3/Assets/Scripts/PlayerControllerA.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerA : MonoBehaviour
+{
+    public float moveSpeedA = 3;
+
+    private Rigidbody myRigidbody;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = new Vector3(Input.GetAxisRaw("HorizontalPlayerA"), 0, Input.GetAxisRaw("VerticalPlayerA")) * moveSpeedA;
+        moveVelocity = moveInput * moveSpeedA;
+    }
+
+
+    private void FixedUpdate()
+    {
+        myRigidbody.velocity = moveVelocity;
+    }
+}
+
+
